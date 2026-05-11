420de76ac28a97c25fb894ac1f94a4f7cdf5df2b
Stuti Shah <shahstuti193@gmail.com>
2026-05-11 18:40:16 +0530

    visamo commit

---
+++ WebApplication3/Controllers/VisitorsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class VisitorsController : Controller
+    {
+        // GET: VisitorsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VisitorsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VisitorsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VisitorsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VisitorsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VisitorsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VisitorsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VisitorsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication3/wwwroot/assets/js/datatable/datatable.init.js
@@ -0,0 +1,24 @@
+$.fn.dataTable.ext.errMode = 'none'; // Suppress DataTables errors
+$(function () {
+  // responsive table
+  $("#config-table").DataTable({
+    responsive: true,
+  });
+
+  $("#all-student").DataTable({
+    columnDefs: [
+      {
+        targets: [0],
+        orderData: [0, 1],
+      },
+      {
+        targets: [1],
+        orderData: [1, 0],
+      },
+      {
+        targets: [4],
+        orderData: [4, 0],
+      },
+    ],
+  });
+});
